d8e46d7305af65b103a4fb75b02dec7b4bf50a46
abuobaid9 <abuobaid.anas1999@gmail.com>
2022-04-17 20:40:23 +0300

    add

---
+++ js/app.js
@@ -0,0 +1,17 @@
+var userName = prompt("Enter Your Name");
+
+
+var userGender = prompt("what is you gender if male type ( m ) if female type ( f )");
+ 
+var userAge = prompt("Enter Your Age")
+if(userAge<=0){
+    alert("Your Age Is Less Than Or Equal To Zero");
+}
+
+
+var welcomeMassges =confirm("If You Want To Skip The Welcoming Massge Press (Ok)");
+
+if(welcomeMassges==false){
+(userGender =="m" )?alert ("Wlecome Mr : " + userName):
+(userGender== "f")? alert("Welcome Ms"+ userName) :
+ alert("Welcome To Our Page" + userName);}
